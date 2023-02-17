5f6a6463b001d3a65dc3527e997b2bfa25a3ab6b
it21918 <79996442+it21918@users.noreply.github.com>
2023-02-17 12:31:08 +0200

    Add files via upload

---
+++ tweets.py
@@ -0,0 +1,21 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+def get_tweets(word, limit=100, until="2023-01-01", since="2020-01-01"):
+    query = "(" + word + ")" + "until:" + until + "since:" + since
+    tweets = []
+    limit = limit
+
+
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        if len(tweets) == limit:
+            break
+        else:
+            tweets.append([tweet.date, tweet.username, tweet.content])
+            
+    df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+    print(df)
+
+    # to save to csv
+    # df.to_csv('tweets.csv')
+    return tweets
